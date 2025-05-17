162e19afbdeda0ae1ff9d2418370548cdb91a559
codewithdaviess <codewithdaviess@gmail.com>
2025-05-17 11:26:23 +0200

    Added the Destinations file

---
+++ my-giant/src/components/Destinations.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Destinations = () => {
+  return (
+    <section className="py-12 px-4 md:px-8 bg-white">
+      <h2 className="text-3xl md:text-4xl font-bold text-center">
+        Popular Destinations
+      </h2>
+    </section>
+  );
+};
+
+export default Destinations;
--- my-giant/src/pages/Homepage.jsx
+++ my-giant/src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import Destinations from '../components/Destinations'
 
 const Homepage = () => {
   return (
    <div >
       <Header />
+      <Destinations />
     </div>
   )
 }
