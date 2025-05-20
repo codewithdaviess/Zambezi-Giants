66826baa52bb1732ab46a1d9fceb422961fad3b5
codewithdaviess <codewithdaviess@gmail.com>
2025-05-20 21:41:03 +0200

    removed the Slider

--- my-giant/src/components/Destinations.jsx
+++ my-giant/src/components/Destinations.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
 import destinationData from "../data/destinationData";
 
 const Destinations = () => {
@@ -10,35 +8,28 @@ const Destinations = () => {
         Popular Destinations
       </h2>
 
-      <Swiper
-        spaceBetween={16}
-        slidesPerView="auto"
-        slidesOffsetBefore={16}
-        slidesOffsetAfter={16}
-      >
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 px-4">
         {destinationData.map((destination, index) => (
-          <SwiperSlide
+          <div
             key={index}
-            className="!w-[70%] sm:!w-[45%] md:!w-[30%] lg:!w-[18%]"
+            className="relative overflow-hidden rounded-lg group h-48"
           >
-            <div className="relative overflow-hidden rounded-lg group h-48">
-              <img
-                src={destination.image}
-                alt={`${destination.city}, ${destination.country}`}
-                className="h-full w-full object-cover transition-transform duration-300 group-hover:scale-110"
-              />
-              {/* Gradient overlay */}
-              <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/30 to-transparent z-10" />
+            <img
+              src={destination.image}
+              alt={`${destination.city}, ${destination.country}`}
+              className="h-full w-full object-cover transition-transform duration-300 group-hover:scale-110"
+            />
+            {/* Gradient overlay */}
+            <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/30 to-transparent z-10" />
 
-              {/* Text overlay */}
-              <div className="absolute bottom-2 left-4 z-20 text-white">
-                <h3 className="text-md font-semibold">{destination.city}</h3>
-                <p className="text-sm">{destination.country}</p>
-              </div>
+            {/* Text overlay */}
+            <div className="absolute bottom-2 left-4 z-20 text-white">
+              <h3 className="text-md font-semibold">{destination.city}</h3>
+              <p className="text-sm">{destination.country}</p>
             </div>
-          </SwiperSlide>
+          </div>
         ))}
-      </Swiper>
+      </div>
     </section>
   );
 };
--- my-giant/src/data/destinationData.jsx
+++ my-giant/src/data/destinationData.jsx
@@ -5,9 +5,6 @@ import img2 from '../assets/img/2.png';
 import img3 from '../assets/img/3.png';
 import img4 from '../assets/img/4.png';
 import img5 from '../assets/img/5.png';
-import img6 from '../assets/img/6.png';
-import img7 from '../assets/img/7.png';
-import img8 from '../assets/img/8.png';
 
 const destinationData = [
   { city: 'Paris', country: 'France', image: img1 },
@@ -15,9 +12,6 @@ const destinationData = [
   { city: 'New York', country: 'USA', image: img3 },
   { city: 'Rome', country: 'Italy', image: img4 },
   { city: 'Sydney', country: 'Australia', image: img5 },
-  { city: 'Cape Town', country: 'South Africa', image: img6 },
-  { city: 'Rio de Janeiro', country: 'Brazil', image: img7 },
-  { city: 'Barcelona', country: 'Spain', image: img8 },
 ];
 
 
